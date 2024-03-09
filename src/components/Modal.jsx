4aaee0d9411e62db1ba4abcccad6ac0e9c9354ab
Khagesh Sharma <khageshsharma24@gmail.com>
2024-03-09 22:12:01 +0530

    Path change

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,7 +19,7 @@ function Modal({ isOpen, onClose, email }) {
       nameOfTool: event.target.nameOfTool.value
     };
     try {
-      const response = await fetch('http://localhost:8000/save_wishlist', {
+      const response = await fetch('https://draftai-backend.onrender.com/save_wishlist', {
         method: 'POST',
         headers: {
           'Access-Control-Allow-Origin': '*',
